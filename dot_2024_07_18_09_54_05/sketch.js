2c596e64a5738b60f5a591f28557f1cdf3bf6f65
PriyankaBansode-ux <priyankabansode196@gmail.com>
2024-07-18 15:28:47 +0530

    add files

---
+++ dot_2024_07_18_09_54_05/sketch.js
@@ -0,0 +1,24 @@
+function setup() {
+  
+  // create a surface to draw in
+ createCanvas(700, 480);
+
+
+  // R, G,& B values.
+  background(21,74,73);
+
+  //take a big brush
+  strokeWeight(10);
+  //dip it in purple colour
+  stroke('yellow');
+  frameRate(10);
+}
+
+function draw() {
+  
+ //declare variables
+  let posY = mouseX;
+  let posX = mouseY;
+  //place a dot on screen 
+  point(posX, posY)
+}
